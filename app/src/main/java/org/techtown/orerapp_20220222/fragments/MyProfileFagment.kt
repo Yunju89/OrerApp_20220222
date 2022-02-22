d2e571ba89e9e2e73fa408996404dafe2f18447d
집윤주 <toyaann@naver.com>
2022-02-22 16:00:10 +0900

    프래그먼트로 돌아왔을때, 첨부한 닉네임반영

--- app/src/main/java/org/techtown/orerapp_20220222/fragments/MyProfileFagment.kt
+++ app/src/main/java/org/techtown/orerapp_20220222/fragments/MyProfileFagment.kt
@@ -1,5 +1,6 @@
 package org.techtown.orerapp_20220222.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -43,6 +44,18 @@ class MyProfileFagment : Fragment () {
     }
 
     fun setValues(){
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQ_CODE_NICKNAME){
+            if(requestCode == Activity.RESULT_OK) {
+                val nickname = data!!.getStringExtra("nick")
+                txtNickname.text = nickname
+            }
+        }
 
     }
 
