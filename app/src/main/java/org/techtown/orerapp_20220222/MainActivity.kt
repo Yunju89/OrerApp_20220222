d22a5958b2b9040a203f4fcd30d6e81fc254cb9c
집윤주 <toyaann@naver.com>
2022-02-22 10:04:52 +0900

    뷰 페이저 어댑터 / 뷰 페이저 연결 작업  - 실행 결과로 화면 이동 가능

--- app/src/main/java/org/techtown/orerapp_20220222/MainActivity.kt
+++ app/src/main/java/org/techtown/orerapp_20220222/MainActivity.kt
@@ -2,10 +2,29 @@ package org.techtown.orerapp_20220222
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.techtown.orerapp_20220222.adapters.MainViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdapter : MainViewPagerAdapter          //자료형만 입력
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    fun setupEvents(){
+    }
+
+    fun setValues(){
+
+        mAdapter = MainViewPagerAdapter(supportFragmentManager)      //프래그먼트매니저 받는
+        mainViewPager.adapter = mAdapter                            //mainViewPager 어댑터 연결
+
     }
 }
