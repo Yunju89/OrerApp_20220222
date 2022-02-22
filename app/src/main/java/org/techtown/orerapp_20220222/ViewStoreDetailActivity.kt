b70039da4c543b395c5f8284f7796cf993792f2a
집윤주 <toyaann@naver.com>
2022-02-22 14:46:50 +0900

    첨부해준 데이터 받아오기  - 멤버변수로 만들어서 활용

--- app/src/main/java/org/techtown/orerapp_20220222/ViewStoreDetailActivity.kt
+++ app/src/main/java/org/techtown/orerapp_20220222/ViewStoreDetailActivity.kt
@@ -2,10 +2,26 @@ package org.techtown.orerapp_20220222
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.techtown.orerapp_20220222.datas.StoreData
 
 class ViewStoreDetailActivity : AppCompatActivity() {
+
+    lateinit var mStoreData : StoreData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
+
+        mStoreData = intent.getSerializableExtra("store") as StoreData
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+    }
+
+    fun setValues(){
+
     }
 }
