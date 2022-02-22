61a99c33567435c00d9afd71fd3c5e7b9e2f3883
집윤주 <toyaann@naver.com>
2022-02-22 09:58:54 +0900

    getCount / GetItem 오버라이딩 + 내용구현

--- app/src/main/java/org/techtown/orerapp_20220222/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/org/techtown/orerapp_20220222/adapters/MainViewPagerAdapter.kt
@@ -1,7 +1,18 @@
 package org.techtown.orerapp_20220222.adapters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import org.techtown.orerapp_20220222.fragments.MyProfileFagment
+import org.techtown.orerapp_20220222.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount() =2
+
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> PizzaStoreListFragment()
+            else -> MyProfileFagment()
+        }
+    }
 }
