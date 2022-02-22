d86e5319ed171ce8778983d573bc28d6f4c50ca4
집윤주 <toyaann@naver.com>
2022-02-22 10:51:09 +0900

    가게 데이터 표현 클래스 추가

---
+++ app/src/main/java/org/techtown/orerapp_20220222/datas/StoreData.kt
@@ -0,0 +1,10 @@
+package org.techtown.orerapp_20220222.datas
+
+class StoreData (
+    val name : String,
+    val rating : Double,
+    val PhoneNum : String,      // Int 저장 시 앞부분 0 이 날라감
+    val logoImageUrl : String,
+
+        ) {
+}
