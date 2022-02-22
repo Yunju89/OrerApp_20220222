39813588cdfcbd14631c84a4b130651edd5ae7de
집윤주 <toyaann@naver.com>
2022-02-22 15:12:42 +0900

    실제 데이터 표시하기

--- app/src/main/java/org/techtown/orerapp_20220222/ViewStoreDetailActivity.kt
+++ app/src/main/java/org/techtown/orerapp_20220222/ViewStoreDetailActivity.kt
@@ -2,7 +2,10 @@ package org.techtown.orerapp_20220222
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 import org.techtown.orerapp_20220222.datas.StoreData
+import java.lang.System.load
 
 class ViewStoreDetailActivity : AppCompatActivity() {
 
@@ -22,6 +25,13 @@ class ViewStoreDetailActivity : AppCompatActivity() {
     }
 
     fun setValues(){
+
+        Glide.with(this).load(mStoreData.logoImageUrl).into(imgLogo)
+
+        txtStoreName.text = mStoreData.name
+        txtPhoneNum.text = mStoreData.PhoneNum
+        ratingBar.rating = mStoreData.rating.toFloat()
+        txtRating.text = "${mStoreData.rating}"
 
     }
 }
