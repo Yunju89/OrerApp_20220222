8776eb3d3fcbda4ab93deaff1fbb3bbda35f1033
집윤주 <toyaann@naver.com>
2022-02-22 09:54:59 +0900

    뷰 페이저 어댑터 클래스 제작중 - 상속 부분 해결

---
+++ app/src/main/java/org/techtown/orerapp_20220222/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,7 @@
+package org.techtown.orerapp_20220222.adapters
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+}
